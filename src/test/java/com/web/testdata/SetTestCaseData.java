55a311b2acc3197c138d134f019623c340c2db69
Shubham Naik <shubhamnaik291@gmail.com>
2025-03-14 19:13:55 +0530

    commit2

--- src/test/java/com/web/testdata/SetTestCaseData.java
+++ src/test/java/com/web/testdata/SetTestCaseData.java
@@ -1,12 +1,18 @@
 package com.web.testdata;
 
+import java.util.Random;
+
 public class SetTestCaseData {
 
     public static TestCaseData CentelonTest() {
         TestCaseData testCaseData = new TestCaseData();
         testCaseData.setFirstName("Shubham");
         testCaseData.setLastName("Naik");
-        testCaseData.setEmailAddress("sn.shubhamnaik4@gmail.com");
+
+        Random random = new Random();
+        char c = (char) (random.nextInt(26) + 'A');
+
+        testCaseData.setEmailAddress("Shubham.naik" + c + "@gmail.com");
         testCaseData.setPassword("Centelon@1203");
         testCaseData.setConfirmPassword("Centelon@1203");
         return testCaseData;
